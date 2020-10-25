aace8104e77aad0ad741f453d4b86dfa52d88e80
KAAAsS <admin@kaaass.net>
2020-10-25 22:16:55 +0800

    Initial commit

---
+++ src/test/java/net/kaaass/se/tasker/TaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kaaass.se.tasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
