c1048fec4bb83b86867ed4d3abe8bf33e95f76a7
KAAAsS <admin@kaaass.net>
2020-10-28 21:35:53 +0800

    用户修改接口体修正

--- src/main/java/net/kaaass/se/tasker/controller/request/UserRequest.java
+++ src/main/java/net/kaaass/se/tasker/controller/request/UserRequest.java
@@ -3,18 +3,17 @@ package net.kaaass.se.tasker.controller.request;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 /**
  * 增加、修改用户请求对象
  */
 @Data
-@NoArgsConstructor
 public class UserRequest {
 
     @NonNull
     private String username;
 
-    // TODO
-    @NonNull
+    @Nullable
     private String password;
 }
